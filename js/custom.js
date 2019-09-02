5397143e409ce05cb7cc59089f4d3767ffe7b1a4
sfarmani <sfarmani12@gmail.com>
2019-09-02 10:25:41 -0700

    test

--- js/custom.js
+++ js/custom.js
@@ -5,20 +5,15 @@ $(function () {
     $('#load_sidebar').load('sidebar.html');
     $('#load_banner').load('banner.html');
     $('#load_footer').load('footer.html');
-    var data = [];
     $.getJSON("json/commands.json", function (json) {
-        console.log(json);
-        data = json;
-    });
-    console.log(data);
-
-    $('#commands').DataTable({
-        data: data,
-        columns: [
-            { title: "Command" },
-            { title: "Usage" },
-            { title: "Description" },
-            { title: "Aliases" }
-        ]
+        $('#commands').DataTable({
+            data: json,
+            columns: [
+                { title: "Command" },
+                { title: "Usage" },
+                { title: "Description" },
+                { title: "Aliases" }
+            ]
+        });
     });
 });
