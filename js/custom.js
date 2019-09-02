41a3d674623f318c727be82ed4401a9f4aab730f
sfarmani <sfarmani12@gmail.com>
2019-09-02 00:34:53 -0700

    testing json loading

--- js/custom.js
+++ js/custom.js
@@ -5,4 +5,16 @@ $(function () {
     $('#load_sidebar').load('sidebar.html');
     $('#load_banner').load('banner.html');
     $('#load_footer').load('footer.html');
+    $.getJSON("../json/commands.json", function (json) {
+        console.log("JSON Data received, name is " + json.name);
+    });
+    $('#commands').DataTable({
+        data: dataSet,
+        columns: [
+            { title: "Command" },
+            { title: "Usage" },
+            { title: "Description" },
+            { title: "Aliases" }
+        ]
+    });
 });
