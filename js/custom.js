1363e7a8b1a1da972a339f59fc2d5613c4561ed5
sfarmani <sfarmani12@gmail.com>
2019-09-01 23:55:12 -0700

    testing datatables

---
+++ js/custom.js
@@ -0,0 +1,8 @@
+
+// Load different parts of the pages
+$(function () {
+    $('#load_headers').load('headers.html');
+    $('#load_sidebar').load('sidebar.html');
+    $('#load_banner').load('banner.html');
+    $('#load_footer').load('footer.html');
+});
