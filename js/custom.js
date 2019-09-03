d70f162567af1bd3c68c5a3dcdf4b01b41bb9b0e
sfarmani <sfarmani12@gmail.com>
2019-09-02 22:37:45 -0700

    take out bullet and let it be sortable

--- js/custom.js
+++ js/custom.js
@@ -2,7 +2,7 @@
 // Load different parts of the pages
 $(function () {
     var trolls = ['[HEALED]', '[Air]'];
-    var bullet = "∴";
+    // var bullet = "∴";
     $('#load_headers').load('headers.html');
     $('#load_sidebar').load('sidebar.html');
     $('#load_banner').load('banner.html');
@@ -46,7 +46,7 @@ $(function () {
                         return data.join(" / ");
                     }
                 },
-                { data: "stats", title: "Stats", searchable: false,
+                { data: "stats", title: "Stats", searchable: true,
                     render: function (data) {
                         if (!data) return "<i>none</i>";
                         let str = [];
@@ -59,36 +59,36 @@ $(function () {
                                     if (isFloat(val)) val = Math.round(val * 10000) / 100;
                                 }
                             }
-                            if (stat == 'damage') str.push(`${bullet} ${plusminus}${val} Damage`);
-                            if (stat == 'armor') str.push(`${bullet} ${plusminus}${val} Armor`);
-                            if (stat == 'mainstat') str.push(`${bullet} ${plusminus}${val} Main Stat`);
-                            if (stat == 'allstat') str.push(`${bullet} ${plusminus}${val} All Stats`);
-                            if (stat == 'strength') str.push(`${bullet} ${plusminus}${val} STR`);
-                            if (stat == 'agility') str.push(`${bullet} ${plusminus}${val} AGI`);
-                            if (stat == 'intelligence') str.push(`${bullet} ${plusminus}${val} INT`);
-                            if (stat == 'hp') str.push(`${bullet} ${plusminus}${val} HP`);
-                            if (stat == 'mp') str.push(`${bullet} ${plusminus}${val} MP`);
-                            if (stat == 'attackspeedpercent') str.push(`${bullet} ${plusminus}${val}% Attack Speed`);
-                            if (stat == 'movespeed') str.push(`${bullet} ${plusminus}${val} Movement Speed`);
-                            if (stat == 'movespeedpercent') str.push(`${bullet} ${plusminus}${val}% Movement Speed`);
-                            if (stat == 'dodgechancepercent') str.push(`${bullet} ${plusminus}${val}% Dodge Chance`);
-                            if (stat == 'skilldamagepercent') str.push(`${bullet} ${plusminus}${val}% Skill Damage`);
-                            if (stat == 'critchancepercent') str.push(`${bullet} ${plusminus}${val}% Crit Chance`);
-                            if (stat == 'critmultiplier') str.push(`${bullet} ${plusminus}${val}x Crit Multiplier`);
-                            if (stat == 'periodicdamagepercent') str.push(`${bullet} ${plusminus}${val}% Periodic Damage`);
-                            if (stat == 'mdpercent') str.push(`${bullet} ${plusminus}${val}% Magic Defense`);
-                            if (stat == 'drpercent') str.push(`${bullet} ${plusminus}${val}% Damage Reduction`);
-                            if (stat == 'dtpercent') str.push(`${bullet} ${plusminus}${val}% Damage Taken`);
-                            if (stat == 'healingpercent') str.push(`${bullet} ${plusminus}${val}% Healing`);
-                            if (stat == 'healreceivedpercent') str.push(`${bullet} ${plusminus}${val}% Healing Received`);
-                            if (stat == 'hpregen') str.push(`${bullet} ${plusminus}${val} HP regen`);
-                            if (stat == 'mpregen') str.push(`${bullet} ${plusminus}${val} MP regen`);
-                            if (stat == 'affinityiwpercent') str.push(`${bullet} ${plusminus}${val}% Ice/Water Affinity`);
-                            if (stat == 'affinityflamepercent') str.push(`${bullet} ${plusminus}${val}% Flame Affinity`);
-                            if (stat == 'affinityearthpercent') str.push(`${bullet} ${plusminus}${val}% Earth Affinity`);
-                            if (stat == 'affinitywlpercent') str.push(`${bullet} ${plusminus}${val}% Wind/Lightning Affinity`);
-                            if (stat == 'expreceivedpercent') str.push(`${bullet} ${plusminus}${val}% EXP Received`);
-                            if (stat == 'revivaltimepercent') str.push(`${bullet} ${plusminus}${val}% Revival Time`);
+                            if (stat == 'damage') str.push(`${plusminus}${val} Damage`);
+                            if (stat == 'armor') str.push(`${plusminus}${val} Armor`);
+                            if (stat == 'mainstat') str.push(`${plusminus}${val} Main Stat`);
+                            if (stat == 'allstat') str.push(`${plusminus}${val} All Stats`);
+                            if (stat == 'strength') str.push(`${plusminus}${val} STR`);
+                            if (stat == 'agility') str.push(`${plusminus}${val} AGI`);
+                            if (stat == 'intelligence') str.push(`${plusminus}${val} INT`);
+                            if (stat == 'hp') str.push(`${plusminus}${val} HP`);
+                            if (stat == 'mp') str.push(`${plusminus}${val} MP`);
+                            if (stat == 'attackspeedpercent') str.push(`${plusminus}${val}% Attack Speed`);
+                            if (stat == 'movespeed') str.push(`${plusminus}${val} Movement Speed`);
+                            if (stat == 'movespeedpercent') str.push(`${plusminus}${val}% Movement Speed`);
+                            if (stat == 'dodgechancepercent') str.push(`${plusminus}${val}% Dodge Chance`);
+                            if (stat == 'skilldamagepercent') str.push(`${plusminus}${val}% Skill Damage`);
+                            if (stat == 'critchancepercent') str.push(`${plusminus}${val}% Crit Chance`);
+                            if (stat == 'critmultiplier') str.push(`${plusminus}${val}x Crit Multiplier`);
+                            if (stat == 'periodicdamagepercent') str.push(`${plusminus}${val}% Periodic Damage`);
+                            if (stat == 'mdpercent') str.push(`${plusminus}${val}% Magic Defense`);
+                            if (stat == 'drpercent') str.push(`${plusminus}${val}% Damage Reduction`);
+                            if (stat == 'dtpercent') str.push(`${plusminus}${val}% Damage Taken`);
+                            if (stat == 'healingpercent') str.push(`${plusminus}${val}% Healing`);
+                            if (stat == 'healreceivedpercent') str.push(`${plusminus}${val}% Healing Received`);
+                            if (stat == 'hpregen') str.push(`${plusminus}${val} HP regen`);
+                            if (stat == 'mpregen') str.push(`${plusminus}${val} MP regen`);
+                            if (stat == 'affinityiwpercent') str.push(`${plusminus}${val}% Ice/Water Affinity`);
+                            if (stat == 'affinityflamepercent') str.push(`${plusminus}${val}% Flame Affinity`);
+                            if (stat == 'affinityearthpercent') str.push(`${plusminus}${val}% Earth Affinity`);
+                            if (stat == 'affinitywlpercent') str.push(`${plusminus}${val}% Wind/Lightning Affinity`);
+                            if (stat == 'expreceivedpercent') str.push(`${plusminus}${val}% EXP Received`);
+                            if (stat == 'revivaltimepercent') str.push(`${plusminus}${val}% Revival Time`);
                         });
                         return str.join('\n')
                     }
