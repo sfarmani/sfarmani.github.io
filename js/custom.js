bd8af1808288022a3e339d56e5fb2b1114850ec8
sfarmani <sfarmani12@gmail.com>
2019-09-02 22:33:15 -0700

    testing stats

--- js/custom.js
+++ js/custom.js
@@ -25,18 +25,78 @@ $(function () {
             columns: [
                 { data: "name", title: "Name", searchable: true },
                 { data: "koreanname", title: "Korean Name", searchable: true, defaultContent: "<i>none</i>" },
-                { data: "droprate", title: "Drop Rate", searchable: false, 
-                render: function(data){
-                    console.log(data);
-                    if (!data) return "<i>none</i>";
-                    data = Math.round(data * 10000) / 100;
-                    return data + "%";
-                } },
+                { data: "droprate", title: "Drop Rate", searchable: false,
+                    render: function (data) {
+                        if (!data) return "<i>none</i>";
+                        data = Math.round(data * 10000) / 100;
+                        return data + "%";
+                    } 
+                },
                 { data: "type", title: "Item Type", searchable: true },
-                { data: "dropped_by", title: "Dropped By", searchable: true, defaultContent: "<i>none</i>" },
-                { data: "required_by", title: "Required By", searchable: false, defaultContent: "<i>none</i>" },
-                { data: "stats", title: "Stats", searchable: false, defaultContent: "<i>none</i>" }
+                { data: "dropped_by", title: "Dropped By", searchable: true,
+                    render: function (data) {
+                        if (!data) return "<i>none</i>";
+                        return data.join(" / ");
+                    }
+                },
+                { data: "required_by", title: "Required By", searchable: false,
+                    render: function (data) {
+                        if (!data) return "<i>none</i>";
+                        return data.join(" / ");
+                    }
+                },
+                { data: "stats", title: "Stats", searchable: false,
+                    render: function (data) {
+                        if (!data) return "<i>none</i>";
+                        let str = [];
+                        Object.keys(data).forEach(function (stat) {
+                            let val = data[stat];
+                            let plusminus = ""
+                            if (Number.isInteger(val) || isFloat(val)) {
+                                if (val >= 0) plusminus = "+";
+                                if (!['critmultiplier', 'hpregen', 'mpregen'].includes(stat)) {
+                                    if (isFloat(val)) val = Math.round(val * 10000) / 100;
+                                }
+                            }
+                            if (stat == 'damage') str.push(`${bullet} ${plusminus}${val} Damage`);
+                            if (stat == 'armor') str.push(`${bullet} ${plusminus}${val} Armor`);
+                            if (stat == 'mainstat') str.push(`${bullet} ${plusminus}${val} Main Stat`);
+                            if (stat == 'allstat') str.push(`${bullet} ${plusminus}${val} All Stats`);
+                            if (stat == 'strength') str.push(`${bullet} ${plusminus}${val} STR`);
+                            if (stat == 'agility') str.push(`${bullet} ${plusminus}${val} AGI`);
+                            if (stat == 'intelligence') str.push(`${bullet} ${plusminus}${val} INT`);
+                            if (stat == 'hp') str.push(`${bullet} ${plusminus}${val} HP`);
+                            if (stat == 'mp') str.push(`${bullet} ${plusminus}${val} MP`);
+                            if (stat == 'attackspeedpercent') str.push(`${bullet} ${plusminus}${val}% Attack Speed`);
+                            if (stat == 'movespeed') str.push(`${bullet} ${plusminus}${val} Movement Speed`);
+                            if (stat == 'movespeedpercent') str.push(`${bullet} ${plusminus}${val}% Movement Speed`);
+                            if (stat == 'dodgechancepercent') str.push(`${bullet} ${plusminus}${val}% Dodge Chance`);
+                            if (stat == 'skilldamagepercent') str.push(`${bullet} ${plusminus}${val}% Skill Damage`);
+                            if (stat == 'critchancepercent') str.push(`${bullet} ${plusminus}${val}% Crit Chance`);
+                            if (stat == 'critmultiplier') str.push(`${bullet} ${plusminus}${val}x Crit Multiplier`);
+                            if (stat == 'periodicdamagepercent') str.push(`${bullet} ${plusminus}${val}% Periodic Damage`);
+                            if (stat == 'mdpercent') str.push(`${bullet} ${plusminus}${val}% Magic Defense`);
+                            if (stat == 'drpercent') str.push(`${bullet} ${plusminus}${val}% Damage Reduction`);
+                            if (stat == 'dtpercent') str.push(`${bullet} ${plusminus}${val}% Damage Taken`);
+                            if (stat == 'healingpercent') str.push(`${bullet} ${plusminus}${val}% Healing`);
+                            if (stat == 'healreceivedpercent') str.push(`${bullet} ${plusminus}${val}% Healing Received`);
+                            if (stat == 'hpregen') str.push(`${bullet} ${plusminus}${val} HP regen`);
+                            if (stat == 'mpregen') str.push(`${bullet} ${plusminus}${val} MP regen`);
+                            if (stat == 'affinityiwpercent') str.push(`${bullet} ${plusminus}${val}% Ice/Water Affinity`);
+                            if (stat == 'affinityflamepercent') str.push(`${bullet} ${plusminus}${val}% Flame Affinity`);
+                            if (stat == 'affinityearthpercent') str.push(`${bullet} ${plusminus}${val}% Earth Affinity`);
+                            if (stat == 'affinitywlpercent') str.push(`${bullet} ${plusminus}${val}% Wind/Lightning Affinity`);
+                            if (stat == 'expreceivedpercent') str.push(`${bullet} ${plusminus}${val}% EXP Received`);
+                            if (stat == 'revivaltimepercent') str.push(`${bullet} ${plusminus}${val}% Revival Time`);
+                        });
+                        return str.join('\n')
+                    }
+                }
             ]
         });
     });
 });
+
+function isFloat(n) {
+    return Number(n) === n && n % 1 !== 0;
+}
