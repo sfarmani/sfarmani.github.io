12dc53ab0fb5a26b57806e8b89e9bc5f69b9ac8c
sfarmani <sfarmani12@gmail.com>
2019-09-09 16:55:11 -0700

    triple equals

--- js/items.js
+++ js/items.js
@@ -186,9 +186,9 @@ $(function () {
                         let str = [];
                         data.forEach(function (rec) {
                             if (Object.keys(rec).length > 1) {
-                                let color0 = json.filter(x => x.name == Object.keys(rec)[0])[0].color
-                                let color1 = json.filter(x => x.name == Object.keys(rec)[1])[0].color
-                                str.push(`<b><u><font color="#${toHex(color0)}">${Object.keys(rec)[0]}</font>/<font color="#${toHex(color1)}">${Object.keys(rec)[1]}</font></u></b>`);
+                                let color0 = json.filter(x => x.name === Object.keys(rec)[0])[0].color
+                                let color1 = json.filter(x => x.name === Object.keys(rec)[1])[0].color
+                                str.push(`<u><font color="#${toHex(color0)}">${Object.keys(rec)[0]}</font>/<font color="#${toHex(color1)}">${Object.keys(rec)[1]}</font></u>`);
                             }
                             else {
                                 let count = rec[Object.keys(rec)] > 1 ? ` <font color="#fff">x</font><font color="#ff8c00">${rec[Object.keys(rec)]}</font>` : '';
