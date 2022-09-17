fc4081cbbc0554d68abf830e5bb18924f340c2ca
sfarmani <sfarmani12@gmail.com>
2022-09-16 22:08:24 -0700

    wait 3 seconds

--- js/block-detect.js
+++ js/block-detect.js
@@ -1,12 +1,16 @@
 $(function () {
 
-    if (!$('.adsbygoogle').not('.adsbygoogle-noablate').children('ins').length || $('.adsbygoogle').not('.adsbygoogle-noablate').is(':hidden')) {
-        $('div#yLBWgYVzweDb').addClass("show-modal");
-        $('body').addClass("modal-open");
-    }
+    setTimeout(
+        function () {
+            if (!$('.adsbygoogle').not('.adsbygoogle-noablate').children('ins').length || $('.adsbygoogle').not('.adsbygoogle-noablate').is(':hidden')) {
+                $('div#yLBWgYVzweDb').addClass("show-modal");
+                $('body').addClass("modal-open");
+            }
 
-    $('.close-button, .ok-button').on('click', function () {
-        $('div#yLBWgYVzweDb').removeClass("show-modal");
-        $('body').removeClass("modal-open");
-    });
+            $('.close-button, .ok-button').on('click', function () {
+                $('div#yLBWgYVzweDb').removeClass("show-modal");
+                $('body').removeClass("modal-open");
+            });
+        },
+    3000);
 });
